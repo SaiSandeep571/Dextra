6cfb3345d76fed900a6113817dfc0bc91263185e
SaiSandeep571 <saisandeepchandra571@gmail.com>
2024-03-29 20:52:32 +0530

    fix

--- lib/homescreen/component/eventcard.dart
+++ lib/homescreen/component/eventcard.dart
@@ -1,122 +1,122 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
-class Eventcard extends StatelessWidget {
-  const Eventcard({super.key});
+// class Eventcard extends StatelessWidget {
+//   const Eventcard({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 129,
-          width: 352,
-          decoration: BoxDecoration(
-            color: Color(0xFF1E1E1E),
-            borderRadius: BorderRadius.circular(10)
-          ),
-          child: Column(
-            children: [
-              Container(
-                height: 24,
-                width: 196,
-                decoration: BoxDecoration(
-                  color: Color(0xFFD9D9D9),
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10),bottomRight: Radius.circular(10))
-                ),
-                child:  Text('Solo Dance',
-                            style: GoogleFonts.poppins(
-                              color: Color(0xFF9C3C3C),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w600
-                            )
-                            ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Text('I . Akleema M Javed  ',
-                                  style: GoogleFonts.poppins(
-                                    color: Color(0xFFFFFFFF),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600
-                                  )
-                                  ),
-                        Text('(S6 CSE) ',
-                                  style: GoogleFonts.poppins(
-                                    color: Color(0xFFFFFFFF),
-                                    fontSize: 9,
-                                    fontWeight: FontWeight.w600
-                                  )
-                                  ),
-                      ],
-                    ),
-                  ),
-                  Image.asset('assets/RUDRA.png',height: 23,width: 23,)            
-                ],
-              ),
-               Row(
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Text('I . Akleema M Javed  ',
-                                  style: GoogleFonts.poppins(
-                                    color: Color(0xFFFFFFFF),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600
-                                  )
-                                  ),
-                        Text('(S6 CSE) ',
-                                  style: GoogleFonts.poppins(
-                                    color: Color(0xFFFFFFFF),
-                                    fontSize: 9,
-                                    fontWeight: FontWeight.w600
-                                  )
-                                  ),
-                      ],
-                    ),
-                  ),
-                  Image.asset('assets/RUDRA.png',height: 23,width: 23,)            
-                ],
-              ),
-               Row(
-                children: [
-                  Expanded(
-                    child: Row(
-                      children: [
-                        Text('I . Akleema M Javed  ',
-                                  style: GoogleFonts.poppins(
-                                    color: Color(0xFFFFFFFF),
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w600
-                                  )
-                                  ),
-                        Text('(S6 CSE) ',
-                                  style: GoogleFonts.poppins(
-                                    color: Color(0xFFFFFFFF),
-                                    fontSize: 9,
-                                    fontWeight: FontWeight.w600
-                                  )
-                                  ),
-                      ],
-                    ),
-                  ),
-                  Image.asset('assets/RUDRA.png',height: 23,width: 23,)            
-                ],
-              ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Container(
+//           height: 129,
+//           width: 352,
+//           decoration: BoxDecoration(
+//             color: Color(0xFF1E1E1E),
+//             borderRadius: BorderRadius.circular(10)
+//           ),
+//           child: Column(
+//             children: [
+//               Container(
+//                 height: 24,
+//                 width: 196,
+//                 decoration: BoxDecoration(
+//                   color: Color(0xFFD9D9D9),
+//                   borderRadius: BorderRadius.only(topLeft: Radius.circular(10),bottomRight: Radius.circular(10))
+//                 ),
+//                 child:  Text('Solo Dance',
+//                             style: GoogleFonts.poppins(
+//                               color: Color(0xFF9C3C3C),
+//                               fontSize: 16,
+//                               fontWeight: FontWeight.w600
+//                             )
+//                             ),
+//               ),
+//               Row(
+//                 children: [
+//                   Expanded(
+//                     child: Row(
+//                       children: [
+//                         Text('I . Akleema M Javed  ',
+//                                   style: GoogleFonts.poppins(
+//                                     color: Color(0xFFFFFFFF),
+//                                     fontSize: 12,
+//                                     fontWeight: FontWeight.w600
+//                                   )
+//                                   ),
+//                         Text('(S6 CSE) ',
+//                                   style: GoogleFonts.poppins(
+//                                     color: Color(0xFFFFFFFF),
+//                                     fontSize: 9,
+//                                     fontWeight: FontWeight.w600
+//                                   )
+//                                   ),
+//                       ],
+//                     ),
+//                   ),
+//                   Image.asset('assets/RUDRA.png',height: 23,width: 23,)            
+//                 ],
+//               ),
+//                Row(
+//                 children: [
+//                   Expanded(
+//                     child: Row(
+//                       children: [
+//                         Text('I . Akleema M Javed  ',
+//                                   style: GoogleFonts.poppins(
+//                                     color: Color(0xFFFFFFFF),
+//                                     fontSize: 12,
+//                                     fontWeight: FontWeight.w600
+//                                   )
+//                                   ),
+//                         Text('(S6 CSE) ',
+//                                   style: GoogleFonts.poppins(
+//                                     color: Color(0xFFFFFFFF),
+//                                     fontSize: 9,
+//                                     fontWeight: FontWeight.w600
+//                                   )
+//                                   ),
+//                       ],
+//                     ),
+//                   ),
+//                   Image.asset('assets/RUDRA.png',height: 23,width: 23,)            
+//                 ],
+//               ),
+//                Row(
+//                 children: [
+//                   Expanded(
+//                     child: Row(
+//                       children: [
+//                         Text('I . Akleema M Javed  ',
+//                                   style: GoogleFonts.poppins(
+//                                     color: Color(0xFFFFFFFF),
+//                                     fontSize: 12,
+//                                     fontWeight: FontWeight.w600
+//                                   )
+//                                   ),
+//                         Text('(S6 CSE) ',
+//                                   style: GoogleFonts.poppins(
+//                                     color: Color(0xFFFFFFFF),
+//                                     fontSize: 9,
+//                                     fontWeight: FontWeight.w600
+//                                   )
+//                                   ),
+//                       ],
+//                     ),
+//                   ),
+//                   Image.asset('assets/RUDRA.png',height: 23,width: 23,)            
+//                 ],
+//               ),
              
-            ],
-          ),
-        ),
+//             ],
+//           ),
+//         ),
         
 
-        SizedBox(height:9)
-      ],
+//         SizedBox(height:9)
+//       ],
 
-    );
-  }
-}
+//     );
+//   }
+// }
--- lib/homescreen/home.dart
+++ lib/homescreen/home.dart
@@ -1,5 +1,4 @@
 import 'package:dextra/homescreen/component/eventcard.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -18,101 +17,108 @@ class _HomescreenState extends State<Homescreen> {
       child: Scaffold(
         backgroundColor: Colors.black,
         body: 
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('DEXTRA 24',
-            style: GoogleFonts.poppins(
-              color: Color(0xFFFFFFFF),
-              fontSize: 14,
-              fontWeight: FontWeight.w700
-            )
-            ),
-
-
-            Text('Leaderboard',
-            style: GoogleFonts.poppins(
-              color: Color(0xFFFFFFFF),
-              fontSize: 20,
-              fontWeight: FontWeight.w700
-            )
-            ),
-
-            Container(
-              height: 223,
-              width: 342,
-              decoration: BoxDecoration(
-                color: Color(0xFF1E1E1E),
-                borderRadius: BorderRadius.circular(8)
+        Container(
+          padding:EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('DEXTRA 24',
+              style: GoogleFonts.poppins(
+                color: Color(0xFFFFFFFF),
+                fontSize: 14,
+                fontWeight: FontWeight.w700
+              )
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          
+          
+              Text('Leaderboard',
+              style: GoogleFonts.poppins(
+                color: Color(0xFFFFFFFF),
+                fontSize: 20,
+                fontWeight: FontWeight.w700
+              )
+              ),
+          
+              Center(
+                child: Container(
+                  height: 223,
+                  width: 342,
+                  decoration: BoxDecoration(
+                    color: Color(0xFF1E1E1E),
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _Rank('assets/rank.png','TEAM RUDRA','HARITHA,RINU','assets/RUDRA.png','54  - P'),
+                      _Rank('assets/rank.png','TEAM AGNI','ANJALI,ARUN','assets/AGNI.png','125  - P'),
+                      _Rank('assets/rank.png','TEAM ASTRA','NANDANA,ABHINAND','assets/ASTRA.png','98  - P'),
+                      _Rank('assets/rank.png','TEAM VAJRA','ARDRA,ADHITH','assets/VAJRA.png','1003  - P')
+                    ],
+                  ),
+                ),
+              ),
+          
+              SizedBox(height: 27),
+          
+              Row(
                 children: [
-                  _Rank('assets/rank.png','TEAM RUDRA','HARITHA,RINU','assets/RUDRA.png','54  - P'),
-                  _Rank('assets/rank.png','TEAM AGNI','ANJALI,ARUN','assets/AGNI.png','54  - P'),
-                  _Rank('assets/rank.png','TEAM ASTRA','NANDANA,ABHINAND','assets/ASTRA.png','54  - P'),
-                  _Rank('assets/rank.png','TEAM VAJRA','ARDRA,ADHITH','assets/VAJRA.png','54  - P')
+                  Text('Event Result',
+                  style: GoogleFonts.poppins(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600
+                  )
+                  ),
+          
+          
+                  SizedBox(width: 35),
+          
+                  Expanded(
+                    child: SizedBox(
+                      height: 28,
+                      width: 192,
+                      child: TextFormField( 
+                        
+                        decoration: InputDecoration(
+                          hintText: 'Search Event',
+                          hintStyle: GoogleFonts.poppins(
+                            color:Color.fromRGBO(255, 255, 255, 0.33),
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400         
+                          ),
+                          filled: true,
+                          fillColor: Color(0xFF1E1E1E),
+                          prefixIcon: Icon(Icons.search_rounded),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8),
+                            borderSide: BorderSide.none
+                          )
+                        ),
+                      ),
+                    ),
+                  )
                 ],
               ),
-            ),
-
-            SizedBox(height: 27),
-
-            Row(
-              children: [
-                Text('Event Result',
-                style: GoogleFonts.poppins(
-                  color: Color(0xFFFFFFFF),
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600
-                )
-                ),
-
-
-                SizedBox(width: 35),
-
-                SizedBox(
-                  height: 28,
-                  width: 192,
-                  child: TextFormField( 
-                    
-                    decoration: InputDecoration(
-                      hintText: 'Search Event',
-                      hintStyle: GoogleFonts.poppins(
-                        color:Color.fromRGBO(255, 255, 255, 0.33),
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400         
-                      ),
-                      filled: true,
-                      fillColor: Color(0xFF1E1E1E),
-                      prefixIcon: Icon(Icons.search_rounded),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide.none
-                      )
-                    ),
-                  ),
-                )
-              ],
-            ),
-
-
-            SizedBox(height: 24),
-
-            Expanded(
-              child: ListView(
-                children: [
-                  
-                  Eventcard(),
-                  Eventcard(),
-                  Eventcard(),
-                  Eventcard(),
-                  ]
-                  ),
-            )
-      
-          ],
           
+          
+              SizedBox(height: 24),
+          
+              Expanded(
+                child: ListView(
+                  children: [
+                    
+                    // Eventcard(),
+                    // Eventcard(),
+                    // Eventcard(),
+                    // Eventcard(),
+                    ]
+                    ),
+              )
+                
+            ],
+            
+          ),
         ),
       ),
     );
@@ -124,7 +130,6 @@ class _HomescreenState extends State<Homescreen> {
 
    _Rank(String rank,teamname,captainname,logo,point,) {
     return Row(
-      crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
 
 
